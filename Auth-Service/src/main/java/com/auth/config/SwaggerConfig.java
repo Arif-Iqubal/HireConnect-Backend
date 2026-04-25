3b3d2f705726494dbf45be495e1beed1b5eb5269
Arif-Iquba'l <arif04102001@gmail.com>
2026-04-25 15:15:58 +0530

    [Arif Iqubal] : enhance Swagger configuration in auth and profile services for API documentation

--- Auth-Service/src/main/java/com/auth/config/SwaggerConfig.java
+++ Auth-Service/src/main/java/com/auth/config/SwaggerConfig.java
@@ -2,6 +2,10 @@ package com.auth.config;
 
 import io.swagger.v3.oas.models.*;
 import io.swagger.v3.oas.models.info.*;
+import io.swagger.v3.oas.models.servers.Server;
+
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,7 +14,12 @@ public class SwaggerConfig {
 
 	@Bean
 	public OpenAPI customOpenAPI() {
-		return new OpenAPI().info(new Info().title("HireConnect Auth Service API") 
+		return new OpenAPI()
+				 // ✅ ADD THIS (IMPORTANT)
+                .servers(List.of(
+                        new Server().url("/auth")
+                ))
+				.info(new Info().title("HireConnect Auth Service API") 
 				.version("1.0").description("Authentication & Authorization APIs"));
 	}
 }
--- Profile-Service/src/main/java/com/profile/config/SwaggerConfig.java
+++ Profile-Service/src/main/java/com/profile/config/SwaggerConfig.java
@@ -3,6 +3,10 @@ package com.profile.config;
 
 import io.swagger.v3.oas.models.*;
 import io.swagger.v3.oas.models.info.*;
+import io.swagger.v3.oas.models.servers.Server;
+
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,7 +15,13 @@ public class SwaggerConfig {
 
 	@Bean
 	public OpenAPI customOpenAPI() {
-		return new OpenAPI().info(new Info().title("HireConnect Profile Service API").version("1.0")
+		return new OpenAPI()
+				 // ✅ ADD THIS (IMPORTANT)
+                .servers(List.of(
+                        new Server().url("/profile")
+                ))
+				
+				.info(new Info().title("HireConnect Profile Service API").version("1.0")
 				.description("Candidate and Recruiter profile APIs"));
 	}
 }
